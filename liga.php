5f30f3388f37be8125df1c8662e061538637ddad
Proxo46 <proxor46@o2.pl>
2021-10-19 11:01:48 +0200

    trzecia czesc zadania

---
+++ liga.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>piłka nożna</title>
+    </head>
+<body>
+    <header id="baner"></header>
+    <h3>Reprezentacja polski w piłce noznej</h3>
+    <ing src="obraz1.jpg" alt="reprezentacja"
+    <section id="lewa"></section>
+    <section id="prawa"></section>
+    <main id="glowny"></main>
+    <section id="glowny"></section>
+
+    "
+</body>
+</html>
