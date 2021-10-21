2237a2d269f713838020b98ffff4c6c40e0e6a31
Proxo46 <proxor46@o2.pl>
2021-10-21 12:19:00 +0200

    zadanie1

--- liga.php
+++ liga.php
@@ -5,16 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>piłka nożna</title>
+    <link rel="stylesheet" href="styl.css">
     </head>
 <body>
-    <header id="baner"></header>
-    <h3>Reprezentacja polski w piłce noznej</h3>
-    <ing src="obraz1.jpg" alt="reprezentacja"
-    <section id="lewa"></section>
-    <section id="prawa"></section>
-    <main id="glowny"></main>
-    <section id="glowny"></section>
+<header>
+        <h3>Reprezentacja Polski w Piłce Nożnej</h3>
+        <img src="obraz1.jpg" alt="reprezentacja">
+    </header>
+    <div id="podzial">
+        <div id="lewy">
+            <form action="liga.php" method="post">
+                <select name="pozycja">
+                    <option value="1">Bramkarze</option>
+                    <option value="2">Obrońcy</option>
+                    <option value="3">Pomocnicy</option>
+                    <option value="4">Napastnicy</option>
+                </select>
+                <button type="submit">Zobacz</button>
+            </form>
+            <img src="zad2.png" alt="piłka">
+            <p>Autor: Daniel Bastek</p>
+        </div>
+        <div id="prawy">
+            <ol>
+               
+            </ol>
+        </div>
+    </div>
+    <main>
+        <h3>Liga mistrzów</h3>
+    </main>
+    <div id="liga">
+    </div>
+   </body>
 
-    "
-</body>
 </html>
